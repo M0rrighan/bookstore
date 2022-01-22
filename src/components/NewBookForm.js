6f5a590963ef2eaa6168ec04d96d0c1807e9dae0
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2022-01-22 12:38:26 +0100

    Edit NewBook component: add functions to submit new book, handle title/author/category updates

--- src/components/NewBookForm.js
+++ src/components/NewBookForm.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import randomId from 'random-id';
+import { addBook } from '../redux/books/books';
 
 function NewBookForm() {
+  const dispatch = useDispatch();
   const categories = [
     'Action and adventure',
     'Alternate history',
@@ -33,26 +37,73 @@ function NewBookForm() {
   ];
 
   const selectOptions = categories.map((cat) => (
-    <option key={cat}>
+    <option key={cat} value={cat}>
       {cat}
     </option>
   ));
 
-  return (
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
 
+  const submitBookToStore = (e) => {
+    e.preventDefault();
+    const newBook = {
+      id: randomId(),
+      title,
+      author,
+      category,
+      progressPercent: 0,
+      chapter: 'Not Updated',
+    };
+
+    // dispatch an action and pass it the newBook object
+    dispatch(addBook(newBook));
+    // reset input fields
+    setTitle('');
+    setAuthor('');
+  };
+
+  const updateTitle = (e) => {
+    setTitle(e.target.value);
+  };
+  const updateAuthor = (e) => {
+    setAuthor(e.target.value);
+  };
+  const updateCategory = (e) => {
+    setCategory(e.target.value);
+  };
+
+  return (
     <div>
       <h2>ADD NEW BOOK</h2>
-      <form>
-        <input type="text" placeholder="Book titile" required />
-        <select required>
-          <option value="" disabled selected hidden>Category</option>
+      <form onSubmit={submitBookToStore}>
+        <input
+          type="text"
+          placeholder="Book titile"
+          onChange={updateTitle}
+          value={title}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Author"
+          onChange={updateAuthor}
+          value={author}
+          required
+        />
+        <select
+          onChange={updateCategory}
+          defaultValue="Unset"
+          required
+        >
+          <option value="Unset" disabled hidden>Category</option>
           {selectOptions}
         </select>
 
-        <button type="submit" className="submitBtn">Add book</button>
+        <button type="submit">Add book</button>
       </form>
     </div>
-
   );
 }
 
