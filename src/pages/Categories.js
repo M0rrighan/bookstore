3690f08b109beba6a1ce8700391624397ced8d43
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2022-01-22 04:26:44 +0100

    Add page Categories.js - Under Construction

---
+++ src/pages/Categories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Categories() {
+  return (
+    <div>
+      <h1 className="text-xl">Under construction</h1>
+    </div>
+  );
+}
+
+export default Categories;
